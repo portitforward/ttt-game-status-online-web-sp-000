7110c6af5517cbddd27ff5ca65b9b60769d27525
Cody Fussell <cody.fussell@gmail.com>
2019-08-26 19:49:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,10 +6,22 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
+<<<<<<< HEAD
 def won?(board)
     WIN_COMBINATIONS.detect{ |winner|
     board[winner[0]] == board[winner[1]] && board[winner[1]] == board[winner[2]] && board[winner[0]] != " "
   }
+=======
+#def won?(board)
+#    WIN_COMBINATIONS.detect{ |winner|
+#    board[winner[0]] == board[winner[1]] && board[winner[1]] == board[winner[2]] && board[winner[0]] != " "
+#  }
+#end  
+
+def won?(board)
+    a = WIN_COMBINATIONS.map.with_index{|con, ind| con.map{|x| x if board[x]=="X" } == con || con.map{|x| x if board[x]=="O" } == con}
+    return a.any?{|x| x if x == true}
+>>>>>>> 95fd55089f54b881a0aedf095d920ef2320685e4
 end
   
 def full?(board)
@@ -22,6 +34,7 @@ def full?(board)
   }
 end
 
+<<<<<<< HEAD
 
 def draw?(board)
   if full?(board) == true && winner(board) == nil
@@ -47,3 +60,16 @@ def winner(board)
     end                
 end 
   
+=======
+def won?(board)
+  WIN_COMBINATIONS.detect{ |winner|
+    board[winner[0]] == board[winner[1]] && board[winner[1]] == board[winner[2]] && board[winner[0]] != " "
+  }
+end
+
+def draw?(board)
+  if full?(board) == true && won?(board) == false
+    return true
+  end
+end
+>>>>>>> 95fd55089f54b881a0aedf095d920ef2320685e4
